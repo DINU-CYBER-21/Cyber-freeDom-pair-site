1ecbdd1dd2b9e7dd46b0d77232d0cae466908e0d
DINU-CYBER-21 <dinuwa280@gmail.com>
2025-08-12 08:47:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
-/*
-_____ _    _ _      _    __  __ ____  
-  / ____| |  | | |    / \  |  \/  |  _ \ 
- | (___ | |  | | |   / _ \ | |\/| | | | |
-  \___ \| |  | | |  / ___ \| |  | | |_| |
-  ____) | |__| | |_/ /   \ \_|  |_|____/ 
- |_____/ \____/|_____/     \_\          
- 
-             S U L A - M D
-*/
-
 import express from 'express';
-import bodyParser from 'body-parser';
 import { fileURLToPath } from 'url';
 import path from 'path';
-
+import { EventEmitter } from 'events';
 import code from './pair.js';
 
-const app = express();
+// Increase EventEmitter limit (investigate if this is truly needed)
+EventEmitter.defaultMaxListeners = 15; // Default is 10, adjust cautiously
 
+const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 const PORT = process.env.PORT || 8000;
 
-import('events').then(events => {
-    events.EventEmitter.defaultMaxListeners = 500;
-});
+// Built-in middleware for parsing JSON and URL-encoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// Serve static files (optional, if you have a public folder)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
 app.use('/code', code);
-app.use('/pair', async (req, res) => {
-    res.sendFile(path.join(__dirname, 'pair.html'));
+app.get('/pair', (req, res, next) => {
+    res.sendFile(path.join(__dirname, 'pair.html'), (err) => {
+        if (err) next(err);
+    });
 });
-app.use('/', async (req, res) => {
-    res.sendFile(path.join(__dirname, 'main.html'));
+app.get('/', (req, res, next) => {
+    res.sendFile(path.join(__dirname, 'main.html'), (err) => {
+        if (err) next(err);
+    });
 });
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// Error-handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
 
 app.listen(PORT, () => {
     console.log(`Powered-by-sula-md-rika-md-cyber-freedom\n\nServer running on http://localhost:${PORT}`);
